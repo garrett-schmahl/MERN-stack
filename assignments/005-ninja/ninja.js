ca7f8271f1339a6fd5fc26c62d09f48f33dd6cef
Garrett Schmahl <gschmahl@pm.me>
2021-05-04 12:25:26 -0700

    super ninja

--- assignments/005-ninja/ninja.js
+++ assignments/005-ninja/ninja.js
@@ -1,9 +1,9 @@
 class Ninja{
-    constructor(name, health){
+    constructor(name, health = 10, speed = 3, strength = 3){
         this.name = name
         this.health = health
-        this.speed = 3
-        this.strength = 3
+        this.speed = speed
+        this.strength = strength
     }
 
     sayName(){
@@ -25,8 +25,29 @@ class Ninja{
     }
 } 
 
-const ninja1 = new Ninja("Hyabusa",10);
+const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+
+class Sensei extends Ninja{
+    constructor(name, wisdom = 10){
+        super(name, 200, 10, 10)
+        this.wisdom = wisdom
+    }
+
+    speakWisdom(){
+        super.drinkSake()
+        console.log("The journey of a thousand miles begins with the first step... to the taxi that will take me to an airport so that I can fly first class. I like it when the stewards put umbrellas in my drink.")
+        return this.health
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
