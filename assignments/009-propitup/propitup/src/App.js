bde382356567c2e8e357dbf5f3a0d604f23c50bf
Garrett Schmahl <gschmahl@pm.me>
2021-05-10 18:30:44 -0700

    putting it together complete

--- assignments/009-propitup/propitup/src/App.js
+++ assignments/009-propitup/propitup/src/App.js
@@ -9,25 +9,25 @@ function App() {
       <CardComponent
         firstName={"Jane"}
         lastName={"Doe"}
-        age={45}
+        startingAge={45}
         hairColor={"Black"}
       />
       <CardComponent
         firstName={"John"}
         lastName={"Smith"}
-        age={88}
+        startingAge={88}
         hairColor={"Brown"}
       />
       <CardComponent
         firstName={"Millard"}
         lastName={"Fillmore"}
-        age={50}
+        startingAge={50}
         hairColor={"Brown"}
       />
       <CardComponent
         firstName={"Maria"}
         lastName={"Smith"}
-        age={62}
+        startingAge={62}
         hairColor={"Brown"}
       />
     </div>
--- assignments/009-propitup/propitup/src/components/cardComponent.js
+++ assignments/009-propitup/propitup/src/components/cardComponent.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 
 class CardComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.startingAge,
+    };
+  }
   render() {
     return (
       <div>
         <h1>
           {this.props.lastName}, {this.props.firstName}
         </h1>
-        <p>Age: {this.props.age}</p>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {this.props.hairColor}</p>
+        <button onClick={this.incrementAge}>Happy Birthday! </button>
       </div>
     );
   }
+  incrementAge = () => {
+    this.setState({ age: this.state.age + 1 });
+    console.log(this.state.age);
+  };
 }
 
 export default CardComponent;
